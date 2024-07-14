303e62cbc031abac5f15416cf3ab8f880e27549a
Bernardo Semião <berna.1991.1@gmail.com>
2024-07-14 20:04:29 +0200

    Created a design based on the euro cup

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -4,14 +4,14 @@ export const Board = ({winner, bingoCard, selected, handleClick}) => {
   return (
     <div
       className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-500 to-indigo-900">
-      {winner ? <p>Bingo</p> : null}
+      {winner && <p className="text-white font-bold">Bingo</p>}
       <div className="grid grid-rows-5 grid-cols-5 gap-2" style={{width: '500px', height: '500px'}}>
         {bingoCard.map((row, rowIndex) => (
           <div key={rowIndex} className="bingo-row">
             {row.map((text, colIndex) => (
               <Square key={rowIndex * 5 + colIndex} square={
                 selected[rowIndex * 5 + colIndex] === 1
-                  ? "bg-green-500" : "bg-white"
+                  ? "square selected" : "square"
               } onClick={() => handleClick(rowIndex, colIndex)}
                       text={text}/>
             ))}
--- src/component/Square.jsx
+++ src/component/Square.jsx
@@ -1,13 +1,18 @@
-
-
 export const Square = ({ text, onClick, val, square }) => {
   return (
     <button
-      className={`${square} flex items-center justify-center border border-gray-300 bg-white text-black rounded-md px-4 py-2 hover:bg-gray-200 transition-colors`}
+      className={`${square} bg-gradient-to-b flex items-center justify-center border border-gray-300 bg-white text-black rounded-md px-4 py-2 transition-colors`}
       onClick={onClick}
-      style={{ width: '100px', height: '100px' }}
+      style={{
+        width: '100px',
+        height: '100px',
+        background: square.includes('selected')
+          ? 'lightgreen'
+          : 'linear-gradient(to bottom, #0928c8, #001d8e)',
+        color: 'white',
+      }}
     >
-      <span className="text-sm">{text}</span>
+      <span className="text-sm font-semibold">{text}</span>
       {val}
     </button>
   );
